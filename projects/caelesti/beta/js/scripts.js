b6edba20c4da2c48584a964f50ae2af1a697a989
SekoiaTree <51149447+SekoiaTree@users.noreply.github.com>
2020-05-11 17:27:23 +0200

    Added comments to javascript code

--- projects/caelesti/beta/js/scripts.js
+++ projects/caelesti/beta/js/scripts.js
@@ -1,17 +1,20 @@
 function download() {
+    //this function unloads the zip file into the data parameter
     JSZipUtils.getBinaryContent('data/caelesti.zip', function (err, data) {
         if (err) {
             throw err;
         }
         var zip = new JSZip();
-        zip.loadAsync(data);
+        zip.loadAsync(data); //this loads it into an editable version
 
+        //and this sends a zipped version to the user
         zip.generateAsync({ type: "blob" })
             .then(function (blob) {
                 saveAs(blob, "caelesti.zip");
             });
     });
 }
+//this doesn't do anything, it's just there for testing purposes
 jQuery(function () {
     $("#demo").html(JSZip.support.blob);
 });
