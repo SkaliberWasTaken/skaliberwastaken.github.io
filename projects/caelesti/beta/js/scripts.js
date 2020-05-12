9f12b919f390f436f92dfeb5aa4cf564bd2d4ad1
SekoiaTree <51149447+SekoiaTree@users.noreply.github.com>
2020-05-12 18:15:29 +0200

    Button now actually sends caelesti to the user
    The download button now actually sends caelesti to the user, instead of an empty zip. There is also a warning because caelesti is very large.

--- projects/caelesti/beta/js/scripts.js
+++ projects/caelesti/beta/js/scripts.js
@@ -1,20 +1,27 @@
-function download() {
-    //this function unloads the zip file into the data parameter
-    JSZipUtils.getBinaryContent('data/caelesti.zip', function (err, data) {
-        if (err) {
-            throw err;
-        }
-        var zip = new JSZip();
-        zip.loadAsync(data); //this loads it into an editable version
 
-        //and this sends a zipped version to the user
-        zip.generateAsync({ type: "blob" })
-            .then(function (blob) {
-                saveAs(blob, "caelesti.zip");
-            });
+function download() {
+    //this part has been pretty much taken from the library's examples, with a couple edits.
+    //this makes a promise (google it if you don't know, too complicated to explain here)
+    var promise = new JSZip.external.Promise(function (resolve, reject) {
+        //gets the content of the adress
+        JSZipUtils.getBinaryContent("https://www.skaliber.net/projects/caelesti/beta/data/caelesti.zip", function (err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
     });
+    //uses the promise to load the data. There is currently no error message if the load fails
+    promise.then(JSZip.loadAsync)
+        .then(function (zip) {
+            $("#download").append("<p>please wait, your download will take some time, as caelesti is very large</p>")
+            zip.generateAsync({ type: "blob" }).then(function (blob) { // generate the zip file
+                saveAs(blob, "caelesti-edit.zip"); // trigger the download
+            }, function (err) {
+                $("#blob").text(err);
+            });
+        });
+
 }
-//this doesn't do anything, it's just there for testing purposes
-jQuery(function () {
-    $("#demo").html(JSZip.support.blob);
-});
+
