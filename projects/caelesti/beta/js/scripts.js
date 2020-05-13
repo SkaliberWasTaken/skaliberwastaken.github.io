a57d6cc0b63a264c1386317c679f05d1b40f5ce3
SekoiaTree <51149447+SekoiaTree@users.noreply.github.com>
2020-05-13 11:42:58 +0200

    Added a progress bar to the download function

--- projects/caelesti/beta/js/scripts.js
+++ projects/caelesti/beta/js/scripts.js
@@ -6,7 +6,7 @@ function download() {
     var hardcore_darkness = true;
     //this part has been pretty much taken from the library's examples, with a couple edits.
     //this makes a promise (google it if you don't know, too complicated to explain here)
-    $("#info").html("unzipping...");
+    $("#info > label").html("unzipping...");
     var promise = new JSZip.external.Promise(function (resolve, reject) {
         //gets the content of the adress
         JSZipUtils.getBinaryContent("https://www.skaliber.net/projects/caelesti/beta/data/caelesti.zip", function (err, data) {
@@ -21,19 +21,30 @@ function download() {
     promise.then(JSZip.loadAsync)
         .then(function (zip) {
             dark_mode_ui = $("#dark-ui").is(':checked');
+
             custom_font = $("#font").is(':checked');
+
             better_leaves = $("#leaves").is(':checked');
+
             hardcore_darkness = $("#darkness").is(':checked');
+
             if (dark_mode_ui) {
                 zip.remove("assets/minecraft/lang");
                 //TODO, do the rest
             }
+
             if (custom_font) {
                 zip.remove("assets/minecraft/textures/font");
             }
-            $("#info").html("rezipping... This can take a while, as there is a lot of content to zip up");
-            zip.generateAsync({ type: "blob" }).then(function (blob) { // generate the zip file
-                $("#info").html("Done!")
+
+            $("#info > label").html("rezipping...");
+
+            zip.generateAsync({ type: "blob" },
+                function updateCallback(metadata) { //basically the progress
+                    $("#info > label")
+                    $("#progress-bar > div").width(metadata.percent + "%");
+            }).then(function (blob) { // generate the zip file
+                $("#info > label").html("Done!")
                 saveAs(blob, "caelesti-edit.zip"); // trigger the download
             }, function (err) {
                 $("#blob").text(err);
@@ -41,4 +52,3 @@ function download() {
         });
 
 }
-
