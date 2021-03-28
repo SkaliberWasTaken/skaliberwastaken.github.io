05f51483bd18a259560e04e29ffbb029070e1140
Magnogen <hydronesium@gmail.com>
2021-03-28 17:46:01 +0100

    namespace support and for more pages
    setting up the groundwork for more pages with this, just by changing it in the page.html file
    
    okay then lets hope this works (fingers crossed)

--- datapackish.js
+++ datapackish.js
@@ -1,20 +1,49 @@
-$.getJSON("data/pages/index.json", function(page_data) {
-  $.getJSON("assets/lang/en_us.json", function(lang_data) {
-    var keys = Object.keys(page_data.contents);
-    for (var index in keys) {
-      if (keys[index].endsWith(":style")) {
-        $("[tag=\"" + keys[index].replace(":style", "") + "\"]").css(parse(page_data.contents[keys[index]], lang_data, "css"));
-      } else {
-        $("[tag=\"" + keys[index] + "\"]").html(parse(page_data.contents[keys[index]], lang_data, "value"));
-      }
-    }
+function perform(s) {
+  s = settings(s);
+  $.getJSON(url(s.page_data, s, "data/[n]/pages/[v].json"), function(page_data) {
+    // alert(JSON.stringify(page_data));
+    $.getJSON(url(s.lang_data, s, "assets/[n]/lang/[v].json"), function(lang_data) {
+      // alert(JSON.stringify(lang_data));
+      $.getJSON(url(parse(page_data.color_palette, lang_data, "value"), s, "assets/[n]/palettes/[v].json"), function(palette_data) {
+        // alert(JSON.stringify(palette_data));
+        var keys = Object.keys(page_data.contents);
+        for (var index in keys) {
+          if (keys[index].endsWith(":style")) {
+            $("[tag=\"" + keys[index].replace(":style", "") + "\"]").css(parse(page_data.contents[keys[index]], lang_data, "css"));
+          } else {
+            $("[tag=\"" + keys[index] + "\"]").html(parse(page_data.contents[keys[index]], lang_data, "value"));
+          }
+        }
+      });
+    });
   });
-});
+}
+
+function url(input, settings, ctx) {
+  var namespace = input.substring(0, input.indexOf(":"));
+  var value = input.substring(input.indexOf(":") + 1);
+  input = settings.content_src + ctx.replace("[n]", namespace).replace("[v]", value);
+  // alert(input);
+  return input;
+}
+
+function settings(settings) {
+  settings = settings || {};
+  settings.use_local = settings.use_local || false;
+  settings.repo = settings.repo || "Magnogen/defaultDatas";
+  settings.path = settings.path || "";
+  settings.page_name = settings.page_name || "index";
+  if (settings.use_local) { settings.content_src = settings.path; }
+  else { settings.content_src = "https://raw.githubusercontent.com/" + settings.repo + "/master/" + settings.path; }
+  return settings;
+}
 
 function parse(param, lang, ctx) {
   var out = "";
   var entryIndex = 0;
-  if (typeof param == "string") { out += param; } else
+  if (typeof param == "string") {
+    out += param;
+  } else
   if (Array.isArray(param)) {
     if (ctx == "rand") { out = param[Math.floor(Math.random() * param.length)]; } else
     if (ctx == "br") {
